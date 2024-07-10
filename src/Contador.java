a23cef01029d9c08cf8a567a8a83d1d8db951db2
Anderson Neves <neves1697@gmail.com>
2024-07-09 21:06:34 -0300

    Criando projeto

---
+++ src/Contador.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Contador {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void contar(int parametro1, int parametro2){
+        System.out.println("Digite o primeiro parâmetro");
+        parametro1 = scanner.nextInt();
+
+        System.out.println("Digite o primeiro parâmetro");
+        parametro2 = scanner.nextInt();
+    }
+
+
+}
